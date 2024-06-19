4880d412f6a2989dbd79953b72fe3a027af3604a
Demmythetechie <demmythetechie@gmail.com>
2024-06-19 20:35:15 +0100

    updated

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import workRoutes from "./routes/work.js";
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use('/work', workRoutes);
 
 app.listen(4000);
